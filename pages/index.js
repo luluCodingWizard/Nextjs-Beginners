2370c25c053f5d88342852d3c5c6e46e0dffa432
lulucodingwizard <how.to.develop.fast@gmail.com>
2022-09-16 14:22:17 +1000

    lesson 5 navigating between pages/routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 // domain.com/
 
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/countries"> get list of countries</Link>
+    </div>
+  );
 }
