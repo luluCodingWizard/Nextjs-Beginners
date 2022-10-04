c18a52fcf98a9204847937803dc03afff6df29a9
lulucodingwizard <how.to.develop.fast@gmail.com>
2022-10-04 16:20:14 +1100

    lesson 11 use getStaticPaths ro prerender the dynamic paths and routes

--- pages/countries/[countryName].js
+++ pages/countries/[countryName].js
@@ -7,3 +7,21 @@ export default function country() {
   // use to query an api call to fetch country details
   return <div>country</div>;
 }
+
+export const getStaticPaths = async () => {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+
+  const paths = data.map((country) => {
+    return {
+      params: {
+        countryName: country.name.common.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
